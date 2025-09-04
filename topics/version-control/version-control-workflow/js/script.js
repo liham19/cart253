2b80a5e22f00d1c73b0454e7fe4ddd3003396f6e
liham19 <liamstg19@gmail.com>
2025-09-04 16:49:35 -0400

    Changed rect position

--- topics/version-control/version-control-workflow/js/script.js
+++ topics/version-control/version-control-workflow/js/script.js
@@ -49,7 +49,7 @@ function draw() {
         // (320, 320) with a size based on the level
         // e.g. level 1 will be a 48x48 rectangle and
         // level 10 will be a 480x480 rectangle
-        rect(580, 802, level * 14, level * 48);
+        rect(610, 520, level * 14, level * 48);
         pop();
     }
 
