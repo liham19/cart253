08da31ac83459d7ec0597e3e7f09bf08b7e68c17
liham19 <liamstg19@gmail.com>
2025-09-30 20:18:40 -0400

    Started the Art Jam

---
+++ topics/art-jam/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Self Portrait Art Jam
+ * Liam St-Georges
+ * 
+ * This is a basic self portrait of myself. Variables will be used to make my eyebrows go up and down. Interactivity will be with the cursor moving my hand. Conditionals will be used to change the hand sign with a simple click of a button :).
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
